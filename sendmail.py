5fcffa69f8c8835a685f7bb985c54289244669a7
mukkarasaikrishnareddy <saikrishnareddymukkara@gmail.com>
2025-08-06 14:49:59 +0530

    updated sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,13 +1,28 @@
 import smtplib
-# creates SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
-# start TLS for security
-s.starttls()
-# Authentication
-s.login("rockstarkid2006@gmail.com","nvlz kzta oirj imrx" )
-# message to be sent
-message = "subject : rey nene ra puka\n\n This is the end hold ur heart breath nowwww"
-# sending the mail
-s.sendmail("rockstarkid2006@gmail.com", "sairockstar2006@gmail.com", message)
-# terminating the session
-s.quit()
+import datetime as dt
+import random
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday <= 6:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quotes = random.choice(all_quotes)
+
+        print(quotes)
+
+        MY_EMAIL = "rockstarkid2006@gmail.com"
+        MY_PASSWORD = "nvlz kzta oirj imrx"
+
+        while True:
+            for quotes in range(0, 1000):
+                with smtplib.SMTP("smtp.gmail.com", 587) as s:
+                    s.starttls()
+                    s.login(MY_EMAIL, MY_PASSWORD)
+                    s.sendmail(
+                        from_addr=MY_EMAIL,
+                        to_addrs="sairockstar2006@gmail.com",
+                        msg=f"Subject: its for you\n\n{quotes}"
+                    )
+
